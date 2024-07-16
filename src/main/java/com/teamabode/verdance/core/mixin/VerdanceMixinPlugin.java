bcef2e9fb1409b4bb16cc16ee15f782956403647
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-07-16 17:38:34 -0500

    Update VerdanceMixinPlugin.java

--- src/main/java/com/teamabode/verdance/core/mixin/VerdanceMixinPlugin.java
+++ src/main/java/com/teamabode/verdance/core/mixin/VerdanceMixinPlugin.java
@@ -1,5 +1,6 @@
 package com.teamabode.verdance.core.mixin;
 
+import com.teamabode.sketch.core.api.config.ConfigManager;
 import com.teamabode.verdance.VerdanceConfig;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
@@ -9,9 +10,14 @@ import java.util.List;
 import java.util.Set;
 
 public class VerdanceMixinPlugin implements IMixinConfigPlugin {
-    public void onLoad(String mixinPackage) {}
+    public void onLoad(String mixinPackage) {
+        // Instead of the main class, we need register at this point.
+        ConfigManager.INSTANCE.register(VerdanceConfig.INSTANCE);
+    }
 
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+
+
         if (mixinClassName.contains("SugarCaneBlock")) {
             return VerdanceConfig.INSTANCE.canBonemealSugarCane.get();
         }
