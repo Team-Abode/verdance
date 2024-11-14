ab7accebac97abeead534276032d8d7988dcb2a2
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-11-14 14:26:51 -0500

    Increase chances of finding cantaloupe in desert villages

--- src/main/java/com/teamabode/verdance/core/misc/VerdanceStructurePoolModifiers.java
+++ src/main/java/com/teamabode/verdance/core/misc/VerdanceStructurePoolModifiers.java
@@ -20,7 +20,7 @@ public class VerdanceStructurePoolModifiers {
             var placedFeatures = registryManager.getWrapperOrThrow(RegistryKeys.PLACED_FEATURE);
 
             modifyStructurePool(Identifier.ofVanilla("village/desert/decor"), registryManager, elements -> {
-                elements.put(StructurePoolElement.ofFeature(placedFeatures.getOrThrow(VerdancePlacedFeatures.PILE_CANTALOUPE)).apply(StructurePool.Projection.RIGID), 4);
+                elements.put(StructurePoolElement.ofFeature(placedFeatures.getOrThrow(VerdancePlacedFeatures.PILE_CANTALOUPE)).apply(StructurePool.Projection.RIGID), 8);
             });
         });
     }
