7b31e9e6368f892fcfaf84a65bcacecdca433fa6
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-11-14 14:23:04 -0500

    Mark elementCounts as mutable

--- src/main/java/com/teamabode/verdance/core/mixin/accessor/StructurePoolAccessor.java
+++ src/main/java/com/teamabode/verdance/core/mixin/accessor/StructurePoolAccessor.java
@@ -5,6 +5,7 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.structure.pool.StructurePool;
 import net.minecraft.structure.pool.StructurePoolElement;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public interface StructurePoolAccessor {
     List<Pair<StructurePoolElement, Integer>> getElementCounts();
 
     @Accessor("elementCounts")
+    @Mutable
     void setElementCounts(List<Pair<StructurePoolElement, Integer>> list);
 
     @Accessor("elements")
